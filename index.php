544080e4df8be542210d0e9f6eb4b2d2bc03e3c8
Simon Prosser <pross@pross.org.uk>
2018-08-28 19:13:29 +0100

    Set default compression to 75 to save some trees.
    Optional 3rd param `compress` to set the compression.

--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 define( 'ABSPATH', dirname( __FILE__ ) );
 $width  = isset( $_GET['width'] )  ? basename( $_GET['width'] )  : 640;
 $height = isset( $_GET['height'] ) ? basename( $_GET['height'] ) : 480;
+$compress = isset( $_GET['compress'] ) ? basename( $_GET['compress'] ) : 75;
 
-// pick a random start image
 $possible_beavers = glob( 'beavers/*.jpg', GLOB_BRACE );
 $random_beaver    = $possible_beavers[ array_rand( $possible_beavers ) ];
 
-$cached_path = sprintf( 'cache/%s-%s-%s.jpg', $width, $height, md5( $random_beaver ) );
+$cached_path = sprintf( 'cache/%s-%s-%s-%s.jpg', $width, $height, $compress, md5( $random_beaver ) );
 
 if ( file_exists( $cached_path ) ) {
 	serve_beaver( $cached_path );
 } else {
-	create_image( $cached_path, $width, $height, $random_beaver );
+	create_image( $cached_path, $width, $height, $random_beaver, $compress );
 	serve_beaver( $cached_path );
 }
 
-function create_image( $cached_path, $newwidth, $newheight, $random_beaver ) {
+function create_image( $cached_path, $newwidth, $newheight, $random_beaver, $compress ) {
 
 	$image_path = ABSPATH . '/' . $random_beaver;
 	$src = imagecreatefromjpeg( $image_path );
@@ -25,7 +25,7 @@ function create_image( $cached_path, $newwidth, $newheight, $random_beaver ) {
 	$tmp = imagecreatetruecolor( $newwidth, $newheight );
 
 	imagecopyresampled( $tmp, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height );
-	imagejpeg( $tmp, $cached_path, 100 );
+	imagejpeg( $tmp, $cached_path, $compress );
 }
 
 function serve_beaver( $path ) {
